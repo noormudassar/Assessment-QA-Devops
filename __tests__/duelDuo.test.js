6f3d4714fc3a3ce434eaa9b34c60596a6b69e1aa
Noor Mudassar <noormudassar97@gmail.com>
2023-07-31 23:22:12 -0400

    duelDuo test

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -15,4 +15,15 @@ describe("Duel Duo tests", () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+
+  test("Draw button finder", async () => {
+    await driver.get("http://localhost:3000");
+    await driver.wait(until.elementLocated(By.id("draw")), 1000);
+  });
+  test(`Check that clicking the Draw button displays the div with id = “choices”`, async () => {
+    await driver.get("http://localhost:3000");
+    await driver.wait(until.elementLocated(By.id("draw")), 5000);
+    await driver.findElement(By.id("draw")).click();
+    await driver.wait(until.elementLocated(By.id("choices")), 5000);
+  });
 });
